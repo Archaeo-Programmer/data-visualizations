d1c682fae22e30f986edcad50076a359bb2c7625
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-11-24 16:25:31 -0800

    Added new code for network figures and heat map. But still a work in progress.

--- 02-graphlayouts/02-graphlayouts.R
+++ 02-graphlayouts/02-graphlayouts.R
@@ -1,58 +1,101 @@
 #install.packages("graphlayouts")
-
 library(igraph)
 library(ggraph)
 library(graphlayouts)
+library(dplyr)
+library(ggplot2)
 
 
+# First, create a random network.
 set.seed(413)
 # Create a network with group structure
-g <- igraph::sample_islands(4,52,0.6,22)
+g <- igraph::sample_islands(4, 52, 0.6, 22)
 g <- igraph::simplify(g)
 # Adding the additional 9, as the list needs to be as long as the vertices of the graph (i.e., v())
-V(g)$grp <- as.character(c(rep(1:9,each=23), 9))
+V(g)$grp <- as.character(c(rep(1:9, each = 23), 9))
 
-# This allows for a little more structure in viewing the network hairball.
-ggraph(g,layout = "stress") +
-  geom_edge_link0(colour=rgb(0,0,0,0.5),width=0.1) +
-  geom_node_point(aes(col=grp)) +
+# The backbone layout allows for a little more structure when viewing a network hairball.
+ggraph(g, layout = "stress") +
+  geom_edge_link0(colour = rgb(0, 0, 0, 0.5), width = 0.1) +
+  geom_node_point(aes(col = grp)) +
   scale_color_brewer(palette = "Set1") +
   theme_graph() +
   theme(legend.position = "none")
 
-# Now, we can also use the backbone layout to discover some groupings.
-bb <- layout_as_backbone(g,keep=0.45)
+# Save image to file.
+ggsave("ggraph_net_stress.png")
+
+
+# Now, we can also use the backbone layout to discover to see possible groupings in the data.
+bb <- layout_as_backbone(g, keep = 0.45)
 E(g)$col <- F
 E(g)$col[bb$backbone] <- T
 
-ggraph(g,layout="manual",x=bb$xy[,1],y=bb$xy[,2])+
-  geom_edge_link0(aes(col=col),width=0.1)+
-  geom_node_point(aes(col=grp))+
-  scale_color_brewer(palette = "Set1")+
-  scale_edge_color_manual(values=c(rgb(0,0,0,0.3),rgb(0,0,0,1)))+
-  theme_graph()+
+ggraph(g,
+       layout = "manual",
+       x = bb$xy[, 1],
+       y = bb$xy[, 2]) +
+  geom_edge_link0(aes(col = col), width = 0.1) +
+  geom_node_point(aes(col = grp)) +
+  scale_color_brewer(palette = "Set1") +
+  scale_edge_color_manual(values = c(rgb(0, 0, 0, 0.3), rgb(0, 0, 0, 1))) +
+  theme_graph() +
   theme(legend.position = "none")
 
+# Save image to file.
+ggsave("ggraph_net_manual.png")
 
 
-# You can also view networks in other ways, such as heatmaps.
-E(g)$weight <- c(rep(1:109,each=30), 9)
-netm <- igraph::get.adjacency(g, attr="weight", sparse=F)
-colnames(netm) <- V(net)$media
-rownames(netm) <- V(net)$media
+# You can also view networks in other ways, such as by heatmaps.
+g.mat <- get.adjacency(g, sparse = FALSE)
 
-palf <- colorRampPalette(rev(RColorBrewer::brewer.pal(11,"RdYlBu")))
-heatmap(netm[,208:1], Rowv = NA, Colv = NA, col = palf(200), labCol = (208:1),
-        scale="none", margins=c(10,13) )
-legend(x="right", legend=c("min edges", "medium edges", "max edges"),fill=palf(50))
+# Change to long format (i.e., an edgelist but including all the 0s).
+longData <- reshape2::melt(g.mat) %>%
+  tidygraph::as_tibble(longData) %>%
+  dplyr::mutate(Var1 = forcats::fct_rev(factor(Var1)))
 
+# using geom_tile
+cols <- RColorBrewer::brewer.pal(3, 'Blues')[c(1, 3)]
 
+ggplot(longData, aes(x = Var2, y = Var1)) +
+  geom_tile(aes(fill = value)) +
+  scale_fill_gradient(low = cols[1], high = cols[2]) +
+  theme_bw() +
+  ggtitle("Heat Map Representation of a Random Network") +
+  xlab("") +
+  ylab("") + #set clean background and no titles
+  guides(fill = guide_colourbar(barheight = 12)) + # can set the length of colour bar
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1),
+    plot.title = element_text(hjust = 0.5),
+    legend.title = element_blank()
+  ) +
+  scale_y_discrete(breaks = seq(0, 200, 25)) +
+  coord_fixed() # fix to square coordinator
 
-edge.start <- ends(g, es=E(g), names=F)[,1]
+# Save image to file.
+ggsave("net_heatmap.png")
+
+# Create a network map with color, curved lines, and groupings. Then, save image.
+edge.start <- ends(g, es = E(g), names = F)[, 1]
 edge.col <- V(g)$grp[edge.start]
 plot.new()
-plot(g, edge.color=edge.col, edge.curved=.1, vertex.color=V(g)$grp, vertex.label=V(g), vertex.label.color="black",
-     vertex.label.cex=.7, main="Random Network")
+
+png("curved_color_net.png")
+
+curved_net <- plot(
+  g,
+  edge.color = edge.col,
+  edge.curved = .1,
+  vertex.color = V(g)$grp,
+  vertex.label = NA,
+  vertex.label.color = "black",
+  vertex.label.cex = .7,
+  main = "Random Network"
+)
+
+print(curved_net)
+dev.off()
 
 
 # library(threejs)
